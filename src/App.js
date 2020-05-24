7b7c28f717c48a54d2eb7d235b18a15ce73766c4
zach <zachcwilliams@icloud.com>
2020-05-23 17:15:12 -0700

    add responsive background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./App.scss";
+// import BackgroundImage from "../public/bg-img.png"
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <div className="red-background"><div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="red-background"></div>
+          <div className="test">
+            <img className="background-image" src={process.env.PUBLIC_URL + './bg-img.png'}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
